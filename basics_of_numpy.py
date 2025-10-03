f569707923166e9ca430ea88b03c15f12a1bc653
Snehamn24 <snehamn216@gmail.com>
2025-10-03 20:43:34 +0530

    Version of numpy and creating ndarray

---
+++ basics_of_numpy.py
@@ -0,0 +1,11 @@
+#First we need to import the numpy module
+
+import numpy as np
+print(np.__version__)#knowing the numpy module version
+
+#numpy is used to create the ndarray using the array()
+
+arr = np.array([1,2,3,4,5,6])
+
+print(arr)
+
