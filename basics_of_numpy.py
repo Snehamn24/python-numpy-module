ae6d271e8c1af5cacdd56b8a183687cd1d91ddd1
Snehamn24 <snehamn216@gmail.com>
2025-10-03 20:50:51 +0530

    Dimensions of array

--- basics_of_numpy.py
+++ basics_of_numpy.py
@@ -5,7 +5,35 @@ print(np.__version__)#knowing the numpy module version
 
 #numpy is used to create the ndarray using the array()
 
+#we can pass the tuple or the list to the array() which converts them to the ndarray
+
 arr = np.array([1,2,3,4,5,6])
 
 print(arr)
+
+print("Type the passed Array is  ")
+
+print(type(arr))
+
+
+#Dimension of the Arrays in ndarray
+
+# 0 - Dimensional
+
+arr1 = np.array(90)
+print("0 Dimension array ")
+print(arr1)
+
+# 1 - Dimesional array
+
+arr2 = np.array([10,20,30])
+print("1-Dimensional Array : ")
+print(arr2)
+
+# 2 - Dimesional array which takes the single dimension array as there elements
+
+arr3 = np.array([[10,20],[30,40]])
+print("2-dimensional array ")
+print(arr3)
+
 
